7dc680ce6d5a4482de277b476bef03cd6e4bc8f5
kaiorod <caiorodrigo2775@gmail.com>
2021-10-29 21:47:53 -0300

    Finaliza ParImpar

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n;
+
+            Console.Beep();
+            Console.Clear();
+
+            Console.Write("Digite um número : ");
+            n = double.Parse(Console.ReadLine());
+
+            if(n % 2 == 0)
+            {
+                Console.Write($"{n} é");
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine(" Par");
+                Console.ResetColor();
+            }
+            else 
+            {
+                Console.Write($"{n} é");
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine(" Ímpar");
+                Console.ResetColor();
+            }
+        }
+    }
+}
